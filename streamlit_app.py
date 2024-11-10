bbee1d88227600e38df9d9c084f45eba4c6571ae
IT Betyár <145707654+itbetyar@users.noreply.github.com>
2024-11-11 00:43:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
-st.write("Starting the app...")  # Diagnostic message
 st.title("IT Betyár Streamlit minta")
+st.subheader("I am a subheader!")
+
 st.write("Sima write elem")
+st.text(" Ez egy sima **text**, mint ilyen a csillagok nem \"bold-olnak\" semmit")
+st.markdown("---")
+
+st.markdown("[Google link](https://www.google.com)")
+
+st.image("kep.jpg", caption="Valami", width=150)
+
+st.audio("zene.mp3")
